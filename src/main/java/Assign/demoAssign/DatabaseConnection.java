5fd08d59a91aacdc610495bad339ff40e7338c76
AnujShakya123 <130181154+AnujShakya123@users.noreply.github.com>
2024-10-18 18:24:18 +0530

    Assign

--- src/main/java/Assign/demoAssign/DatabaseConnection.java
+++ src/main/java/Assign/demoAssign/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/registration";
     private static final String USER = "root"; // Replace with your MySQL username
-    private static final String PASSWORD = "Anuj@8645"; // Replace with your MySQL password
+    private static final String PASSWORD = ""; // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
